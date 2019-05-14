ae66a1394bbb5908b2ad300daf9b76895c2cc676
CasSumler <cnsumler17@gmail.com>
2019-05-14 15:49:33 -0500

    Initial Check In

---
+++ C2/Total/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int x;
+        int y;
+        int total;
+
+        x = 3;
+        y = 4;
+        total = x + y;
+
+        System.out.println("total = " + total);
+    }
+}
