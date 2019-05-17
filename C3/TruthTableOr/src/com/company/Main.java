81a1123ce04b043e9b7c7ecf35cf80e5a8a7f4dd
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:24:23 -0500

    initial check in

---
+++ C3/TruthTableOr/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("      Truth Table");
+        System.out.println(" a       b   | a || b");
+        System.out.println("---------------------");
+        System.out.println("false false  | " + (b || b));
+        System.out.println("false true   | " + (b || a));
+        System.out.println("true false   | " + (a || b));
+        System.out.println("true true    | " + (a || a));
+    }
+}
