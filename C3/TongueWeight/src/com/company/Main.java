2beecd0fa381a3d1439343413b719e5a83461339
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:35:56 -0500

    initial check in

---
+++ C3/TongueWeight/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        java.util.Scanner userInput = new java.util.Scanner(System.in);
+
+        System.out.println("Please enter the trailer weight: " );
+        double trailerWeight = userInput.nextDouble();
+
+        System.out.println("Please enter the cargo weight: ");
+        double cargoWeight = userInput.nextDouble();
+
+        double totalWeight = trailerWeight + cargoWeight;
+        double minTongueWeight = totalWeight * 0.09;
+        double maxTongueWeight = totalWeight * 0.15;
+
+        System.out.println("The minimum tongue weight allowed is: " + minTongueWeight + " pounds.");
+        System.out.println("The maximum tongue weight allowed is: " + maxTongueWeight + " pounds.");
+
+
+
+    }
+}
