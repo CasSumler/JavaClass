be5e031ed41880132ac5df01bb3ccd2ad08bb2fa
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:40:07 -0500

    initial check in

---
+++ C4/EvenOrOdd/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        java.util.Scanner userInput = new java.util.Scanner(System.in);
+
+        System.out.println("Please enter number here: ");
+        int number = userInput.nextInt();
+
+        if (number % 2 == 0)
+        {
+            System.out.println("The number is even.");
+        }
+        else
+        {
+            System.out.println("The number is odd.");
+        }
+
+    }
+}
