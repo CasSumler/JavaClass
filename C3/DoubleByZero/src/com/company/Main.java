e8f7d827f20aa755d77d67010649838f26501238
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:32:10 -0500

    initial check in

---
+++ C3/DoubleByZero/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double x = 42.0;
+        double y = 0.0;
+
+        double result = x / y;
+        System.out.println(result);
+    }
+}
