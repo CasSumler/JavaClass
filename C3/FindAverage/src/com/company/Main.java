5b0576bfe8983b6ce2004971acf39a191c07b7f4
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:21:58 -0500

    initial check in

---
+++ C3/FindAverage/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int total = (5 + 6 + 7 + 12);
+        double result = total / 4.0;
+
+        System.out.println("The average is: " + result);
+    }
+}
