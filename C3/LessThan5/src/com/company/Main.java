d1c498333b8e86fca1a7f1605adb42febfc6f820
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:25:39 -0500

    initial check in

---
+++ C3/LessThan5/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int x = 0;
+        boolean lessThan5 = x < 5;
+        System.out.println(lessThan5);
+
+        x = 1;
+        lessThan5 = x < 5;
+        System.out.println(lessThan5);
+
+        x = 4;
+        lessThan5 = x < 5;
+        System.out.println(lessThan5);
+
+        x = 5;
+        lessThan5 = x < 5;
+        System.out.println(lessThan5);
+
+        x = 6;
+        lessThan5 = x < 5;
+        System.out.println(lessThan5);
+
+        x = 10;
+        lessThan5 = x < 5;
+        System.out.println(lessThan5);
+    }
+}
