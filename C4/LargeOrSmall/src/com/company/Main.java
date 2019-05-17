d7df9f5952cc79341b6a0305b366bd7fb8f59e45
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:39:27 -0500

    initial check in

---
+++ C4/LargeOrSmall/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        java.util.Scanner userInput = new java.util.Scanner(System.in);
+
+        System.out.println("Please enter number here: ");
+        int number = userInput.nextInt();
+
+        if (number > 99)
+        {
+            System.out.println("The number is large.");
+        }
+        else if (number < 10)
+        {
+            System.out.println("The number is small.");
+        }
+        else
+        {
+            System.out.println("The number is neither large nor small.");
+        }
+
+
+    }
+}
