7c12db18f04194fce37bd4fc732fca3e9ef74202
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:26:58 -0500

    initial check in

---
+++ C3/InsideRange/src/com/company/Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int x = 0;
+        boolean b1 = x > 0;
+        boolean b2 = x < 6;
+        System.out.println(b1 && b2);
+
+        x = 1;
+        b1 = x > 0;
+        b2 = x < 6;
+        System.out.println(b1 && b2);
+
+        x = 4;
+        b1 = x > 0;
+        b2 = x < 6;
+        System.out.println(b1 && b2);
+
+        x = 5;
+        b1 = x > 0;
+        b2 = x < 6;
+        System.out.println(b1 && b2);
+
+        x = 6;
+        b1 = x > 0;
+        b2 = x < 6;
+        System.out.println(b1 && b2);
+
+        x = 10;
+        b1 = x > 0;
+        b2 = x < 6;
+        System.out.println(b1 && b2);
+
+
+
+
+
+
+    }
+}
