d58e1939dd933503d3bae130f64684dcc7b5c11f
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:47:31 -0500

    initial check in

---
+++ C4/PrintBy1s/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int count = 1;
+
+        while (count <= 10)
+        {
+            System.out.println(count);
+            count++;
+        }
+
+        count = 10;
+        while (count >= 1)
+        {
+            System.out.println(count);
+            count--;
+        }
+    }
+}
