ec6536a68c06021c53c60aa3fa5e2bde496a316d
CasSumler <cnsumler17@gmail.com>
2019-05-17 16:32:53 -0500

    initial check in

---
+++ C3/LoveJava/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        String one = "I";
+        String two = "love";
+        String three = "Java.";
+
+        System.out.println(one + " " + two + " " + three);
+    }
+}
