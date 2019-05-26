86ce1850181f629e75bb341130541c4c4384b9c0
CasSumler <cnsumler17@gmail.com>
2019-05-26 16:13:47 -0500

    initial check in

--- C5/Sums/src/com/company/Main.java
+++ C5/Sums/src/com/company/Main.java
@@ -7,11 +7,18 @@ public class Main
     {
         printSum(4, 6);
         printSum(7, 2);
+        printSum(1,2,3,4,5);
     }
 
     private static void printSum(int a, int b)
     {
         int sum = a + b;
+        System.out.println("The sum is: " + sum);
+    }
+
+    private static void printSum(int a, int b, int c, int d, int e)
+    {
+        int sum = a + b + c + d + e;
         System.out.println("The sum is: " + sum);
     }
 }
