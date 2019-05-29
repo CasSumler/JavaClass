dda11378cd245a927a123b66b39e302ab59e2b79
CasSumler <cnsumler17@gmail.com>
2019-05-29 10:26:05 -0500

    initial check in

---
+++ C6/HomeBuilder/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        House house = new House(5);
+        System.out.println("Number of rooms in the house: " + house.getRoomCount());
+
+        //house.roomCount = 7;
+    }
+}
