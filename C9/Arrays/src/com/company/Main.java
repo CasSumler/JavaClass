c6dd2f8099b67af3f969a052e5240c621d36e926
CasSumler <cnsumler17@gmail.com>
2019-05-31 11:03:36 -0500

    initial check in

---
+++ C9/Arrays/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int[] practice = new int[3];
+        practice[0] = 1;
+        practice[1] = 5;
+        practice[2] = 9;
+    }
+}
